e0817686b28bea73b58961a59d2ab6b2391a72fa
DrBlackRat <fabian.rose.207@outlook.de>
2025-01-22 00:07:11 +0100

    Added Master functionality

--- Packages/xyz.drblackrat.pictureloader/Runtime/Scripts/PictureLoaderPersistence.cs
+++ Packages/xyz.drblackrat.pictureloader/Runtime/Scripts/PictureLoaderPersistence.cs
@@ -9,21 +9,34 @@ namespace DrBlackRat.VRC.PictureLoader
     public class PictureLoaderPersistence : UdonSharpBehaviour
     {
         [Header("Settings")]
+        [Tooltip("Use Networking Master instead of Instance Owner. Instance Owner only works in Invite, Invite+, Friends and Friends+, but has some usage benefits.")]
+        [SerializeField] private bool useNetMaster;
         [Tooltip("List of Picture Loader URL Inputs that you want to use Persistence for")]
         [SerializeField] private PictureLoaderURLInput[] urlInputs;
         [UdonSynced] private VRCUrl[] urls;
 
         private bool isLocalOwner;
         private bool setupCorrect;
+        
+        // The edition of "useNetMaster" later on made this code a bit cursed, it should probably be refactored at some point
         private void Start()
         {
-            // Cache Networking Owner
             var owner = Networking.GetOwner(gameObject);
-            // Check if Persistence should be used for the owner etc
-            PLDebug.PersistenceLog(owner.isInstanceOwner
-                ? $"Running Persistence on {owner.displayName}'s client."
-                : $"Skipping Persistence for {owner.displayName} as they are not the Instance Owner.");
-            isLocalOwner = Networking.LocalPlayer.isInstanceOwner && owner.isLocal;
+            if (!useNetMaster)
+            {
+                PLDebug.PersistenceLog(owner.isInstanceOwner
+                    ? $"Running Persistence on {owner.displayName}'s client."
+                    : $"Skipping Persistence for {owner.displayName} as they are not the Instance Owner.");
+                isLocalOwner = Networking.LocalPlayer.isInstanceOwner && owner.isLocal;
+            }
+            else
+            {
+                PLDebug.PersistenceLog(owner.isMaster
+                    ? $"{owner.displayName} is the current Master. Persistence may or may not be running on their client."
+                    : $"Skipping Persistence for {owner.displayName} as they are not the Master.");
+                isLocalOwner = Networking.LocalPlayer.isMaster && owner.isLocal;
+            }
+
             if (!isLocalOwner) return;
             setupCorrect = CheckSetup();
             if (!setupCorrect) return;
@@ -49,6 +62,26 @@ namespace DrBlackRat.VRC.PictureLoader
                 }
             }
             return true;
+        }
+        
+        public override void OnPlayerLeft(VRCPlayerApi player)
+        {
+            if (!useNetMaster) return;
+            
+            var owner = Networking.GetOwner(gameObject);
+            if (!owner.isMaster) return;
+            PLDebug.PersistenceLog($"{owner.displayName} is now Master. They can now save URLs.");
+            isLocalOwner = owner.isLocal;
+            
+            // Check setup in case it hasn't been checked already & do inital setup on current client
+            if (!isLocalOwner || setupCorrect) return;
+            setupCorrect = CheckSetup();
+            if (!setupCorrect) return;
+            for (int i = 0; i < urlInputs.Length; i++)
+            {
+                urlInputs[i]._SetPersistenceReference(i, this);
+            }
+            
         }
         public override void OnDeserialization()
         {
